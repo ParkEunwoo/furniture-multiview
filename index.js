1b07b6c6f4d90f939a8cbde3b8e759cee85c5150
ParkEunwoo <dmsdn960@gmail.com>
2020-01-04 01:53:02 +0900

    add multer

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
+const multer = require("multer");
+// const upload = multer({ dest: "furniture/" });
 
 app.use(express.static("public"));
+
+app.post("/upload", function(req, res) {
+  res.send("success");
+});
 
 app.listen(3000, () => {
   console.log("listening 3000 port");
